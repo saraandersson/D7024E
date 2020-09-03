6df244ddf2f250a302d6ec9875e3c9ffc5daa464
saraandersson <saraandersson>
2020-09-03 14:46:08 +0200

    testing message

--- udpclient.go
+++ udpclient.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("udp", "127.0.0.1:1234")
-	if err != nil {
-		// handle error
-	}
-	fmt.Fprintf(conn, "Hello")
-	status, err := bufio.NewReader(conn).ReadString('\n')
+    p :=  make([]byte, 2048)
+    conn, err := net.Dial("udp", "127.0.0.1:1234")
+    if err != nil {
+        fmt.Printf("Some error %v", err)
+        return
+    }
+    fmt.Fprintf(conn, "Hi UDP Server, How are you doing?")
+    _, err = bufio.NewReader(conn).Read(p)
+    if err == nil {
+        fmt.Printf("%s\n", p)
+    } else {
+        fmt.Printf("Some error %v\n", err)
+    }
+    conn.Close()
 }
