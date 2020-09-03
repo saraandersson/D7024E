553fa653767d0bb90bd880d3507e7861301641a3
saraandersson <saraandersson>
2020-09-03 15:03:22 +0200

    testing message

--- udpclient.go
+++ udpclient.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     p :=  make([]byte, 2048)
-    conn, err := net.Dial("udp", "127.0.0.1:1234")
+    conn, err := net.DialUDP("udp", "127.0.0.1:1234")
     if err != nil {
         fmt.Printf("Some error %v", err)
         return
