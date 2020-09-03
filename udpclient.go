5544f0a33c535f6834bf7ac4d68a5f5145e0d190
fannyblixt <fanny-blixt@hotmail.com>
2020-09-03 09:09:44 +0200

    testing sending messages

---
+++ udpclient.go
@@ -0,0 +1,33 @@
+package D7024E
+import ( 
+    "net"  
+)
+
+func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
+    _,err := conn.WriteToUDP([]byte("World"), addr)
+    if err != nil {
+        fmt.Printf("Couldn't send response %v", err)
+    }
+}
+
+func main() {
+	p := make([]byte, 2048)
+    addr := net.UDPAddr{
+        Port: 1234,
+        IP: net.ParseIP("127.0.0.1"),
+    }
+    ser, err := net.ListenUDP("udp", &addr)
+    if err != nil {
+        fmt.Printf("Some error %v\n", err)
+        return
+    }
+    for {
+        _,remoteaddr,err := ser.ReadFromUDP(p)
+        fmt.Printf("Read a message from %v %s \n", remoteaddr, p)
+        if err !=  nil {
+            fmt.Printf("Some error  %v", err)
+            continue
+        }
+        go sendResponse(ser, remoteaddr)
+    }
+}
---
+++ udpserver.go
@@ -0,0 +1,13 @@
+package D7024E
+import (
+    "net"
+)
+
+func main() {
+	conn, err := net.Dial("udp", "127.0.0.1:1234")
+	if err != nil {
+		// handle error
+	}
+	fmt.Fprintf(conn, "Hello")
+	status, err := bufio.NewReader(conn).ReadString('\n')
+}
