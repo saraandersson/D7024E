3c529c0c22027431222c74f42f3319502a2b8964
fannyblixt <fanny-blixt@hotmail.com>
2020-09-04 11:34:30 +0200

    test

--- main.go
+++ main.go
@@ -1,48 +1,48 @@
 package main
 import (
-	"fmt"
-	"net"
-	"time"
+    "fmt"
+    "net"
+    "time"
 )
 
 func main() {
-	go mainServer() //Starting mainServer as a thred
-	<- time.After(5*time.Second)  //Timer, continue after 5 second
-    conn, err := net.Dial("udp", "127.0.0.1:1234") //Sending UDP request
+    go mainServer() //Gör egen tråd
+    <- time.After(5*time.Second)
+    conn, err := net.Dial("udp", "127.0.0.1:1234")
     if err != nil {
-        fmt.Printf("Error: Error when sending request, %v", err)
+        fmt.Printf("ERROR: %v", err)
         return
-	}
-	fmt.Printf("Sending request.")
-    fmt.Fprintf(conn, "Hello")  //Sending "Hello" as a request
+    }
+    fmt.Printf("Hit kommer jag!")
+    fmt.Fprintf(conn, "Hello")
     conn.Close()
 }
 
 func mainServer() {
-	p := make([]byte, 2048)
+    p := make([]byte, 2048)
     addr := net.UDPAddr{
         Port: 1234,
         IP: net.ParseIP("127.0.0.1"),
     }
-    ser, err := net.ListenUDP("udp", &addr) //Listen after UDP request
+    ser, err := net.ListenUDP("udp", &addr)
     if err != nil {
-        fmt.Printf("Error: Error when reciving request, %v", err)
+        fmt.Printf("ERROR %v\n", err)
         return
-	}
+    }
     for {
-		_,remoteaddr,err := ser.ReadFromUDP(p) //Reading request message
-		fmt.Print(p) 
+        ,remoteaddr,err := ser.ReadFromUDP(p)
+        fmt.Print(p)
         if err != nil {
-            fmt.Printf("Error: Error when reading request, %v", err)
+            fmt.Printf("ERROR %v\n", err)
             return
         }
-        go sendResponse(ser, remoteaddr)  //Call sendResponse function as a thread
+        go sendResponse(ser, remoteaddr)
     }
 }
 func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
-    _,err := conn.WriteToUDP([]byte("World"), addr)
+    ,err := conn.WriteToUDP([]byte("World"), addr)
     if err != nil {
-        fmt.Printf("Error: Error when sending response, %v", err)
-	}
-	fmt.Printf("Response sending")
+        fmt.Printf("ERROR SERVER: %v", err)
+    }
+    fmt.Printf("Response sending")
 }
