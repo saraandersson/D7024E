8bc12d2a342fb31f6412bb852b63127e277c0ae4
fannyblixt <fanny-blixt@hotmail.com>
2020-09-04 11:27:41 +0200

    test

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,4 @@
-#FROM ubuntu:18.04
 
-#RUN apt-get update
-#RUN apt-get -y upgrade
-#RUN apt-get -y install golang
 # Add the commands needed to put your compiled go binary in the container and
 # run it when the container starts.
 #
@@ -12,46 +8,13 @@
 # In order to use this file together with the docker-compose.yml file in the
 # same directory, you need to ensure the image you build gets the name
 # "kadlab", which you do by using the following command:
-#
-# $ docker build . -t kadlab
-#WORKDIR /app
-
-#COPY . /home/go/src/D7024E
-
-#WORKDIR /home/go/src/D7024E
-
-#COPY . .
-#COPY . /go/src/project/
-
-#RUN echo "PWD is: $PWD"
 
 FROM golang:alpine
 WORKDIR /source
-#WORKDIR /source/server
-#COPY udpserver.go /source/server
-#RUN go build udpserver.go
-#RUN go run udpserver.go
-#WORKDIR /source/client
-COPY udpclient.go /source
-RUN go build udpclient.go
-#RUN go run udpclient.go
-#CMD ./udpserver
-CMD ./udpclient
+COPY main.go /source
+RUN go build main.go
+CMD ./main
 
 
 
-
-#ADD ./D7024E /image
-
-#CMD go run udpclient.go
-
-#RUN go build
-
-#RUN go build -o /D7024E
-
-#RUN go run main.go config.go server.go
-
-#CMD ["./main"]
-
-#CMD ["./D7024E/udpclient"]
 
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+func main() {
+	go mainServer() //Starting mainServer as a thred
+	<- time.After(1*time.Second)  //Timer, continue after 1 second
+    conn, err := net.Dial("udp", "127.0.0.1:1234") //Sending UDP request
+    if err != nil {
+        fmt.Printf("Error: Error when sending request, %v", err)
+        return
+	}
+	fmt.Printf("Sending request.")
+    fmt.Fprintf(conn, "Hello")  //Sending "Hello" as a request
+    conn.Close()
+}
+
+func mainServer() {
+	p := make([]byte, 2048)
+    addr := net.UDPAddr{
+        Port: 1234,
+        IP: net.ParseIP("127.0.0.1"),
+    }
+    ser, err := net.ListenUDP("udp", &addr) //Listen after UDP request
+    if err != nil {
+        fmt.Printf("Error: Error when reciving request, %v", err)
+        return
+	}
+    for {
+		_,remoteaddr,err := ser.ReadFromUDP(p) //Reading request message
+		fmt.Println(p) 
+        if err != nil {
+            fmt.Printf("Error: Error when reading request, %v", err)
+            return
+        }
+        go sendResponse(ser, remoteaddr)  //Call sendResponse function as a thread
+    }
+}
+func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
+    _,err := conn.WriteToUDP([]byte("World"), addr)
+    if err != nil {
+        fmt.Printf("Error: Error when sending response, %v", err)
+	}
+	fmt.Printf("Response sending")
+}
