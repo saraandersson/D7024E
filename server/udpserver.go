3f508e3109e687e266ac857e6c8d02cbf6b92d37
saraandersson <saraandersson>
2020-09-07 09:42:33 +0200

    testar med channels

--- server/udpserver.go
+++ server/udpserver.go
@@ -3,30 +3,12 @@ package main
 import (
 	"fmt"
     "net"
-    "time"
-    "os"
 )
 
 func main() {
-    done := make(chan bool)
-    fmt.Printf(os.Args)
-	go mainServer(done) //Gör egen tråd
-	<- time.After(1*time.Second)
-    conn, err := net.Dial("udp", "127.0.0.1:1234")
-    if err != nil {
-        fmt.Printf("ERROR: %v", err)
-        return
-	}
-	fmt.Printf("Hit kommer jag!")
-    fmt.Fprintf(conn, "Hello")
-    defer conn.Close()
-    <-done
-}
-
-func mainServer(done chan bool) {
 	p := make([]byte, 2048)
     addr := net.UDPAddr{
-        Port: 1234,
+        Port: 8001,
         IP: net.ParseIP("127.0.0.1"),
     }
     ser, err := net.ListenUDP("udp", &addr)
@@ -40,14 +22,13 @@ func mainServer(done chan bool) {
             fmt.Printf("ERROR %v\n", err)
             return
         }
-        go sendResponse(ser, remoteaddr, done)
+        go sendResponse(ser, remoteaddr)
     }
 }
-func sendResponse(conn *net.UDPConn, addr *net.UDPAddr, done chan bool) {
+func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
     _,err := conn.WriteToUDP([]byte("World"), addr)
     if err != nil {
         fmt.Printf("ERROR SERVER: %v", err)
     }
     fmt.Printf("Response sending")
-    done <- true
 }
