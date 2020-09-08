ddc5cac146948d513588525b8c8f26814e3785e3
saraandersson <saraandersson>
2020-09-08 10:43:44 +0200

    testar med channels

--- udpclient.go
+++ udpclient.go
@@ -16,16 +16,77 @@ type Node struct {
 
 func main() {
     numberOfNodes := 4;
-    port := 8000;
+    startingPort := 8000;
     newPort := 0;
+    port := os.Getenv("PORT")
     for i := 0; i < numberOfNodes; i++ {
         fmt.Println("Enter for-loop")
-        newPort = port + i;
+        newPort = startingPort + i;
         newNode := createNewNode("localhost:" + strconv.Itoa(newPort))
-        go newNode.checkNodeIsUp()
-        
+        go newNode.checkNodeIsUp() 
+    }
+    go mainServer(port)
+    conn, err := net.Dial("udp", "127.0.0.1:" + port)
+    if err != nil {
+        fmt.Printf("ERROR: %v", err)
+        return
+	}
+	fmt.Printf("Send request")
+    fmt.Fprintf(conn, "Hello")
+    defer conn.Close()
+}
+
+func mainServer(port int) {
+	p := make([]byte, 2048)
+    addr := net.UDPAddr{
+        Port: port,
+        IP: net.ParseIP("localhost"),
+    }
+    ser, err := net.ListenUDP("udp", &addr)
+    if err != nil {
+        fmt.Println("ERROR %v\n", err)
+        return
+    }
+    for {
+        _,remoteaddr,err := ser.ReadFromUDP(p)
+        if err != nil {
+            fmt.Println("ERROR %v\n", err)
+            return
+        }
+        go sendResponse(ser, remoteaddr)
     }
 }
+func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
+    _,err := conn.WriteToUDP([]byte("World"), addr)
+    if err != nil {
+        fmt.Println("ERROR SERVER: %v", err)
+    }
+    fmt.Println("Request received")
+}
+
+/*
+
+func main() {
+    numberOfNodes := 4;
+    startingPort := 8000;
+    newPort := 0;
+    port := os.Getenv("PORT")
+    for i := 0; i < numberOfNodes; i++ {
+        fmt.Println("Enter for-loop")
+        newPort = startingPort + i;
+        newNode := createNewNode("localhost:" + strconv.Itoa(newPort))
+        go newNode.checkNodeIsUp() 
+    }
+    conn, err := net.Dial("udp", "localhost:" + strconv.Itoa(port))
+    if err != nil {
+        fmt.Printf("ERROR: %v", err)
+        return
+    }
+    fmt.Fprintf(conn, "Hello")
+
+
+}
+
 
 func createNewNode(address string) *Node{
     fmt.Println("Enter createNewNode")
@@ -43,56 +104,5 @@ func createNewNode(address string) *Node{
 
 func (node *Node) checkNodeIsUp() {
     fmt.Println("Hello I am a new node existing on: " + node.address)
-    defer node.connection.Close()
-}
-
-/*func main() {
-    done := make(chan bool)
-    //fmt.Println("port: ", os.Getenv("PORT"))
-    //portOwn := os.Getenv("PORTOWN")
-    portSending := os.Getenv("PORTSENDING")
-    i2, err1 := strconv.Atoi(portSending)
-    if err1 != nil {
-        go mainServer(done, i2) //Gör egen tråd
-    }
-    go mainServer(done)
-	<- time.After(1*time.Second)
-    conn, err := net.Dial("udp", "127.0.0.1:" + portSending)
-    if err != nil {
-        fmt.Printf("ERROR: %v", err)
-        return
-	}
-	fmt.Printf("Send request")
-    fmt.Fprintf(conn, "Hello")
-    defer conn.Close()
-    <-done
+    //defer node.connection.Close()
 }*/
-
-func mainServer(done chan bool) {
-	p := make([]byte, 2048)
-    addr := net.UDPAddr{
-        Port: 8000,
-        IP: net.ParseIP("localhost"),
-    }
-    ser, err := net.ListenUDP("udp", &addr)
-    if err != nil {
-        fmt.Println("ERROR %v\n", err)
-        return
-    }
-    for {
-        _,remoteaddr,err := ser.ReadFromUDP(p)
-        if err != nil {
-            fmt.Println("ERROR %v\n", err)
-            return
-        }
-        go sendResponse(ser, remoteaddr, done)
-    }
-}
-func sendResponse(conn *net.UDPConn, addr *net.UDPAddr, done chan bool) {
-    _,err := conn.WriteToUDP([]byte("World"), addr)
-    if err != nil {
-        fmt.Println("ERROR SERVER: %v", err)
-    }
-    fmt.Println("Request received")
-    done <- true
-}
