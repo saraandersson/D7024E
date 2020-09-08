f5f3f11cb61110a1c9873a060adf718906aeb182
fannyblixt <fanny-blixt@hotmail.com>
2020-09-08 14:18:54 +0200

    testing server client

--- udpclient.go
+++ udpclient.go
@@ -17,7 +17,7 @@ func main() {
         CONNECT := arguments[1]
         */
 
-        s, err := net.ResolveUDPAddr("udp4", "127.0.0.1:1235")
+        s, err := net.ResolveUDPAddr("udp4", "127.0.0.1:1236")
         c, err := net.DialUDP("udp4", nil, s)
         if err != nil {
                 fmt.Println(err)
