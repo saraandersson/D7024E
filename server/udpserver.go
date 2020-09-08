7f68ec642f5175b34019fa5b92a7796e6984cb94
fannyblixt <fanny-blixt@hotmail.com>
2020-09-08 15:28:31 +0200

    testing server client

--- server/udpserver.go
+++ server/udpserver.go
@@ -16,7 +16,6 @@ type Node struct {
 func main() {
     numberOfNodes := 2;
     port := 8000;
-    newPort := 0;
     for i := 0; i < numberOfNodes; i++ {
         //fmt.Println("Enter for-loop")
         newPort := port + i
