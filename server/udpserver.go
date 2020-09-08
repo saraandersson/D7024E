02eae3f4c825699b7a79d77d50f15a287b991daf
fannyblixt <fanny-blixt@hotmail.com>
2020-09-08 15:20:44 +0200

    testing server client

--- server/udpserver.go
+++ server/udpserver.go
@@ -7,34 +7,59 @@ import (
         "bufio"
 )
 
+type Node struct {
+    address string
+    connection *net.UDPConn
+}
+
 func main() {
-        port_input := os.Getenv("PORT")
-        PORT := ":" + port_input
-        s, err := net.ResolveUDPAddr("udp4", PORT)
+    numberOfNodes := 2;
+    port := 8000;
+    for i := 0; i < numberOfNodes; i++ {
+        //fmt.Println("Enter for-loop")
+        newNode := createNewNode("localhost:" + strconv.Atoi(port))
+       // go newNode.checkNodeIsUp()
+        go newNode.testListen()
+        
+    }
+}
+
+func createNewNode(address string) *Node{
+        s, err := net.ResolveUDPAddr("udp", address)
         if err != nil {
                 fmt.Println(err)
-                return
+                
         }
-        connection, err := net.ListenUDP("udp4", s)
+        connection, err := net.ListenUDP("udp", s)
         if err != nil {
                 fmt.Println(err)
-                return
+                
         }
-        defer connection.Close()
-        buffer := make([]byte, 1024)
+        return &Node{address, connection}
+}
+
+func (node *Node) testListen(){
+    buffer := make([]byte, 1024)
 
         for {
-                n, addr, err := connection.ReadFromUDP(buffer)
+                n, addr, err := node.connection.ReadFromUDP(buffer)
                 fmt.Print("Message: ", string(buffer[0:n-1]))
-                reader := bufio.NewReader(os.Stdin)
-                fmt.Print("Type answer here: ")
-                text, _ := reader.ReadString('\n')
-                data := []byte(text + "\n")
-                _, err = connection.WriteToUDP(data, addr)
+               // reader := bufio.NewReader(os.Stdin)
+               // fmt.Print("Type answer here: ")
+               // text, _ := reader.ReadString('\n')
+                data := []byte("WORLD!" + "\n")
+                _, err = node.connection.WriteToUDP(data, addr)
                 if err != nil {
                         fmt.Println(err)
-                        return
+                        
                 }
         }
+        defer node.connection.Close()
+}
+
+
+func (node *Node) checkNodeIsUp() {
+    fmt.Println("Hello I am a new node existing on: " + node.address)
+    defer node.connection.Close()
 }
     
