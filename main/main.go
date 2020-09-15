b407917e01546a7ac622af75f775910fc4f268bf
saraandersson <saraandersson>
2020-09-15 15:43:32 +0200

    implementing ping

--- main/main.go
+++ main/main.go
@@ -19,21 +19,23 @@ func main() {
 	fmt.Println(contact)
 	network := d7024e.CreateNetwork(&contact)
 	routingTable := d7024e.NewRoutingTable(contact)
-	reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Type operation here: ")
-	text, _ := reader.ReadString('\n')
-	if text == "ping" {
-		network.SendPingMessage(&contact)
-	}
-	if text == "join network" {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+    	fmt.Print("Type operation here: ")
+		text, _ := reader.ReadString('\n')
+		if text == "ping" {
+			network.SendPingMessage(&contact)
+		}
+		if text == "join network" {
 
-	}
-	if text == "node lookup" {
-		/*fmt.Print("Enter targetNode id: ")
-		targetNode, _ := reader.ReadString('\n')
-		//var convertToKademliaId d7024e.KademliaID
-		targetNodeId := []byte(targetNode)*/
-		contacts := routingTable.FindClosestContacts(id, 1)
-		fmt.Println(contacts)
+		}
+		if text == "node lookup" {
+			/*fmt.Print("Enter targetNode id: ")
+			targetNode, _ := reader.ReadString('\n')
+			//var convertToKademliaId d7024e.KademliaID
+			targetNodeId := []byte(targetNode)*/
+			contacts := routingTable.FindClosestContacts(id, 1)
+			fmt.Println(contacts)
+		}
 	}
 }
