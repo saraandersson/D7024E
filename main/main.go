019e110f1e40bc3068828ac9f6ba7ab984a6d4f0
saraandersson <saraandersson>
2020-09-15 10:52:49 +0200

    implementing ping

--- main/main.go
+++ main/main.go
@@ -14,7 +14,7 @@ func main() {
 	var node d7024e.Contact
 	address := "127.0.0.1:1234"
 	id := d7024e.NewRandomKademliaID()
-	node := d7024e.NewContact(id, address)
+	node = d7024e.NewContact(id, address)
 	fmt.Println(id)
 	fmt.Println(node)
 	network := d7024e.CreateNetwork()
