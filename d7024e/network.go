f8297b000d9f48d5a286bd0e0a7ab316cb4ff60d
fannyblixt <fanny-blixt@hotmail.com>
2020-09-17 08:46:17 +0200

    testing

--- d7024e/network.go
+++ d7024e/network.go
@@ -88,8 +88,8 @@ func (network *Network) SendStoreMessage(data []byte) {
 func NewNetwork(contact *Contact, bootstrapContact *Contact) Network {
 	network := Network{}
 	network.contact=contact
-	network.routingTable= NewRoutingTable(contact)
-	go network.SendPingMessage(&bootstrapContact)
+	network.routingTable= NewRoutingTable(&contact)
+	go network.SendPingMessage(bootstrapContact)
 	return network
 }
 
