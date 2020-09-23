2cb143052fa782654fe22a730e08c9154b678eaf
fannyblixt <fanny-blixt@hotmail.com>
2020-09-23 08:47:15 +0200

    edit

--- main/main.go
+++ main/main.go
@@ -16,6 +16,15 @@ import (
 const defaultPort ="8000"
 
 func main() {
+
+        /*
+        rt.AddContact(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001"))
+	rt.AddContact(NewContact(NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002"))
+	rt.AddContact(NewContact(NewKademliaID("1111111200000000000000000000000000000000"), "localhost:8002"))
+	rt.AddContact(NewContact(NewKademliaID("1111111300000000000000000000000000000000"), "localhost:8002"))
+	rt.AddContact(NewContact(NewKademliaID("1111111400000000000000000000000000000000"), "localhost:8002"))
+	rt.AddContact(NewContact(NewKademliaID("2111111400000000000000000000000000000000"), "localhost:8002"))
+        */
         done := make(chan bool)
         var port = flag.String("port", defaultPort,"Test")
         var bootstrapIP = flag.String("bootstrap_ip", "kademliaBootstrapHost", "Test")
@@ -26,18 +35,47 @@ func main() {
         address := GetIPContainer() + ":" + *port
         fmt.Println("addressen for noden: ")
         fmt.Println(address)
-        contact = d7024e.NewContact(d7024e.NewRandomKademliaID(), address)
-        routingTableContact := d7024e.NewRoutingTable(contact)
+        /*Add contacts and create routing tables*/
+        bootstrapContact :=  d7024e.NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
+        contact1 := d7024e.NewContact(NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002"))
+        contact2 := d7024e.NewContact(NewKademliaID("1111111200000000000000000000000000000000"), "localhost:8002"))
+        contact3 := d7024e.NewContact(NewKademliaID("1111111300000000000000000000000000000000"), "localhost:8002"))
+        contact4 := d7024e.NewContact(NewKademliaID("1111111400000000000000000000000000000000"), "localhost:8002"))
+        contact5 := d7024e.NewContact(NewKademliaID("2111111400000000000000000000000000000000"), "localhost:8002"))
+        
+        rtContact1 := d7024e.NewRoutingTable(contact1)
+        rtContact2 := d7024e.NewRoutingTable(contact2)
+        rtContact3 := d7024e.NewRoutingTable(contact3)
+        rtContact4 := d7024e.NewRoutingTable(contact4)
+        rtContact5 := d7024e.NewRoutingTable(contact5)
+        rtBootstrap := d7024e.NewRoutingTable(bootstrapContact)
+
+        /* */ 
+        
+        //contact = d7024e.NewContact(d7024e.NewRandomKademliaID(), address)
+        //routingTableContact := d7024e.NewRoutingTable(contact)
+
         /*create network and kademlianetwork*/        
-        bootstrapAddress := *bootstrapIP +":"+ *bootstrapPort
-        bootstrapContact := d7024e.NewContact(d7024e.NewRandomKademliaID(), bootstrapAddress)
-        network := d7024e.NewNetwork(bootstrapContact)
-        bootstrapRoutingTable := d7024e.NewRoutingTable(bootstrapContact)
-        kademliaNetwork := d7024e.NewKademlia(&network, &bootstrapContact, bootstrapRoutingTable, 20, 3, done)
+       // bootstrapAddress := *bootstrapIP +":"+ *bootstrapPort
+        //bootstrapContact := d7024e.NewContact(d7024e.NewRandomKademliaID(), bootstrapAddress)
+       // network := d7024e.NewNetwork(bootstrapContact)
+        //bootstrapRoutingTable := d7024e.NewRoutingTable(bootstrapContact)
+        //kademliaNetwork := d7024e.NewKademlia(&network, &bootstrapContact, bootstrapRoutingTable, 20, 3, done)
         /*Call on LookUpContact*/
-        i, _ := strconv.Atoi(*port)
-        go kademliaNetwork.LookupContact(&contact, routingTableContact, i)
-        network.Listen(address, i)
+       // i, _ := strconv.Atoi(*port)
+       //go kademliaNetwork.LookupContact(routingTableContact, i)
+       // network.Listen(address, i)
+
+       /**/
+        network := d7024e.NewNetwork(bootstrapContact)
+        kademliaNetwork := d7024e.NewKademlia(&network, &bootstrapContact, rtBootstrap, 20, 3, done)
+        go network.Listen("localhost:8000", 8000)
+        kademliaNetwork.LookUpContact(rtContact1, 8002)
+        kademliaNetwork.LookUpContact(rtContact2, 8002)
+        kademliaNetwork.LookUpContact(rtContact3, 8002)
+        kademliaNetwork.LookUpContact(rtContact4, 8002)
+        kademliaNetwork.LookUpContact(rtContact5, 8002)
+       /**/
         <- time.After(1*time.Second)
         fmt.Println("Här kommer listan:")
         <- done
