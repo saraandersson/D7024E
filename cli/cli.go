7368dd10a778716ea506dee40954c159de96dcd1
saraandersson <saraandersson>
2020-09-23 15:53:05 +0200

    add alpha variable

--- cli/cli.go
+++ cli/cli.go
@@ -10,18 +10,28 @@ import (
 func main() {
 
   reader := bufio.NewReader(os.Stdin)
-  fmt.Println("Simple Shell")
+  fmt.Println("Type operation below, you can choose between following: store, find, put, get, exit")
   fmt.Println("---------------------")
 
   for {
     fmt.Print("-> ")
     text, _ := reader.ReadString('\n')
-    // convert CRLF to LF
-    text = strings.Replace(text, "\n", "", -1)
-
-    if strings.Compare("hi", text) == 0 {
-      fmt.Println("hello, Yourself")
-    }
+	// convert CRLF to LF
+	
+	switch text {
+		case "store":
+			fmt.Println("enter store")
+		case "find":
+			fmt.Println("enter find")
+		case "put":
+			fmt.Println("enter put")
+		case "get":
+			fmt.Println("enter get")
+		case "exit":
+			fmt.Println("enter exit")
+		default:
+			fmt.Println("Please type correct operation, you can choose between following: store, find, put, get, exit")
+	}
 
   }
 
