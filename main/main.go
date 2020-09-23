30e99b20623f8535dc5f93f82acd23222ae8f264
saraandersson <saraandersson>
2020-09-23 14:19:04 +0200

    add alpha variable

--- main/main.go
+++ main/main.go
@@ -87,20 +87,14 @@ func main() {
 
         /*Command line interface*/
 
-        //names := make([]string, 0)
-
-        scanner := bufio.NewScanner(os.Stdin)
-    
-        for {
-                fmt.Print("Enter name: ")
-                
-                scanner.Scan()
-                
-                text := scanner.Text()
-
-                if text == "store" {
-                        fmt.Println("enter store")
-                }
+        reader := bufio.NewReader(os.Stdin)
+        fmt.Println("Enter command: ")
+	for {
+                Input, _ := reader.ReadString('\n')
+                fmt.Println(input)
+                //option2 := words[0]
+                //fmt.Println(text)
+		//if strings.TrimRight(text, "\n") == "store" {
         }
 
        /**/
