ce660080e32afaf122f692a266a021adb94caf54
saraandersson <saraandersson>
2020-09-23 14:37:18 +0200

    add alpha variable

--- main/main.go
+++ main/main.go
@@ -89,7 +89,7 @@ func main() {
 
         reader := bufio.NewReader(os.Stdin)
         fmt.Print("Enter text: ")
-        text, _ := reader.ReadString('\n')
+        text, _ := reader.ReadString("\n")
         fmt.Println(text)
 
         /*reader := bufio.NewReader(os.Stdin)
